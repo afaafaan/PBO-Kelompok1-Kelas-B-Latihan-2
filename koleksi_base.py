58db52eb8ffbb9ee34368845c4891ec86ee1c2a7
dafasyahrul <dafasyahrul10@student.uns.ac.id>
2026-06-02 09:15:15 +0700

    Update koleksi_base.py

--- koleksi_base.py
+++ koleksi_base.py
@@ -6,9 +6,6 @@ class Koleksi(ABC):
         self.judul = judul
         self.tahun_terbit = tahun_terbit
         self.penerbit = penerbit
-
-    # ABSTRAKSI & POLIMORFISME: 
-    # Semua kelas anak wajib mengimplementasikan fungsi ini dengan cara mereka sendiri
     @abstractmethod
     def tampil_info(self):
         pass
