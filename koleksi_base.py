5d4e358c8c3c22cf829aaf041717891bd280767e
dafasyahrul <dafasyahrul10@student.uns.ac.id>
2026-06-02 09:38:45 +0700

    Update koleksi_base.py

--- koleksi_base.py
+++ koleksi_base.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
 class Koleksi(ABC):
-    def _init_(self, kode, judul, tahun_terbit, penerbit):
+    def __init__(self, kode, judul, tahun_terbit, penerbit):
         self.kode = kode
         self.judul = judul
         self.tahun_terbit = tahun_terbit
