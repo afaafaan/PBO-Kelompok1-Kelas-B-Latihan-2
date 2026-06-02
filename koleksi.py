024fd57c0283e803eeb5349c2e1bd223d19b7851
muzzz <muznimubarok385@gmail.com>
2026-06-02 08:48:27 +0700

    Menambahkan class Buku dan Majalah (Muzni)

---
+++ koleksi.py
@@ -0,0 +1,20 @@
+class Buku(Koleksi):
+    def _init_(self, kode, judul, tahun_terbit, penerbit, pengarang):
+        super()._init_(kode, judul, tahun_terbit, penerbit)
+        self.pengarang = pengarang
+
+    def tampil_info(self):
+        print(f"Thn Terbit    : {self.tahun_terbit}")
+        print(f"Pengarang     : {self.pengarang}")
+        print(f"Penerbit      : {self.penerbit}")
+
+
+class Majalah(Koleksi):
+    def _init_(self, kode, judul, tahun_terbit, penerbit, edisi):
+        super()._init_(kode, judul, tahun_terbit, penerbit)
+        self.edisi = edisi
+
+    def tampil_info(self):
+        print(f"Tahun Terbit  : {self.tahun_terbit}")
+        print(f"Penerbit      : {self.penerbit}")
+        print(f"Edisi         : {self.edisi}")
